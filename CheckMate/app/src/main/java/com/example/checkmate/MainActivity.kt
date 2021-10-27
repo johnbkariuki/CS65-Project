ba732887a4cbed780124bc8a87d5b5798f58f587
Brandon Feng <brandon.y.feng.22@dartmouth.edu>
2021-10-27 17:08:19 -0400

    adding action bar tabs

--- CheckMate/app/src/main/java/com/example/checkmate/MainActivity.kt
+++ CheckMate/app/src/main/java/com/example/checkmate/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.checkmate
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // setting up bottom navigation menu
+        val bottomNavigationView = findViewById<BottomNavigationView
+                >(R.id.bottom_navigation_view)
+        val navController = findNavController(R.id.nav_fragment)
+        bottomNavigationView.setupWithNavController(
+            navController
+        )
     }
 }
---
+++ CheckMate/app/src/main/java/com/example/checkmate/PaymentActivity.kt
@@ -0,0 +1,27 @@
+package com.example.checkmate
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class PaymentActivity : AppCompatActivity() {
+
+    companion object {
+        // toast messages
+        const val RECEIPT_SUBMITTED_TOAST = "Receipt Submitted"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_payment)
+    }
+
+    // for when user submits receipt
+    fun onSubmitReceipt(view: View) {
+
+        // display toast and exit activity
+        Toast.makeText(this, RECEIPT_SUBMITTED_TOAST, Toast.LENGTH_SHORT).show()
+        finish()
+    }
+}
