beb24c051886aca92b36aa6766cf22537c007f6d
Brandon Feng <brandon.y.feng.22@dartmouth.edu>
2021-11-01 17:22:04 -0400

    formatting receipt

--- CheckMate/app/src/main/java/com/example/checkmate/ReceiptActivity.kt
+++ CheckMate/app/src/main/java/com/example/checkmate/ReceiptActivity.kt
@@ -12,10 +12,7 @@ import android.os.Environment
 import android.provider.MediaStore
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.LinearLayout
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContract
@@ -29,15 +26,26 @@ import com.google.mlkit.vision.text.TextRecognizer
 import com.google.mlkit.vision.text.latin.TextRecognizerOptions
 import com.theartofdev.edmodo.cropper.CropImage
 import java.io.File
+import android.widget.CheckedTextView
+
+
+
 
 class ReceiptActivity : AppCompatActivity() {
     private lateinit var recognizer: TextRecognizer
     private lateinit var receiptImage: InputImage
-    private lateinit var linearLayout: LinearLayout
+
+    private lateinit var receiptListView: ListView
+    private var receiptList = ArrayList<Pair<String, Float>>()
+    private lateinit var adapter: ReceiptListAdapter
 
     companion object {
         // toast messages
         const val RECEIPT_SUBMITTED_TOAST = "Receipt Submitted"
+
+        // receipt display parameters
+        const val RECEIPT_MARGIN = 15
+        const val RECEIPT_TEXT_SIZE = 16F
     }
 
     private val cropActivityResultContract = object: ActivityResultContract<Any,Uri?>(){
@@ -57,14 +65,34 @@ class ReceiptActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_receipt)
+        receiptListView = findViewById<ListView>(R.id.receiptList)
 
-        linearLayout = findViewById<LinearLayout>(R.id.receiptInfo)
+        // creating checklist to format receipt
+        adapter = ReceiptListAdapter(this, receiptList)
+        receiptListView.adapter = adapter
+        // when item on receipt list is clicked
+        receiptListView.setOnItemClickListener { adapterView, view, i, l ->
+            // check item
+            val checkedTextView = view as CheckedTextView
+            checkedTextView.isChecked = !checkedTextView.isChecked
+        }
 
+        // launching camera
         recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS)
         getReceipt()
     }
 
-    //    what to do with photo after taken
+    fun showPopupMenu(view: View) {
+        PopupMenu(view.context, view).apply {
+            menuInflater.inflate(R.menu.popup_menu, menu)
+            setOnMenuItemClickListener { item ->
+                Toast.makeText(view.context, "You Clicked : " + item.title, Toast.LENGTH_SHORT).show()
+                true
+            }
+        }.show()
+    }
+
+    // taking photo
     fun getReceipt() {
         cropActivityResultLauncher = registerForActivityResult(cropActivityResultContract){
             it?.let{ uri ->
@@ -76,12 +104,12 @@ class ReceiptActivity : AppCompatActivity() {
         cropActivityResultLauncher.launch(null)
     }
 
-
+    // processing photo
     fun runTextRecognition(){
         if (this::receiptImage.isInitialized){
             recognizer.process(receiptImage).addOnSuccessListener {
                 println("debug: text recognition success")
-                processTextRecognition(it)
+                displayReceipt(it)
             }.addOnFailureListener{
                 it.printStackTrace()
                 println("debug: text recognition failure")
@@ -89,22 +117,26 @@ class ReceiptActivity : AppCompatActivity() {
         }
     }
 
-    fun processTextRecognition(text: Text){
+    // displaying photo
+    fun displayReceipt(text: Text){
+
         val blocks = text.textBlocks
-        println("size:${blocks.size}")
-        for (block in blocks){
-            println(block.text)
-            println("---------------------")
-            for (line in block.lines){
-                val info = TextView(this)
-                info.setText(line.text)
-                info.setTextSize(16F)
-                val params = LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT)
-                params.setMargins(15, 15, 15, 15)
-                info.layoutParams = params
-                linearLayout.addView(info)
+        // loop through text blocks
+        for (i in 0 until blocks.size) {
+            val block = blocks[i]
+            for (line in block.lines) {
+                // if on receipt line item
+                if (line.text[0] == '$') {
+                    // get item and price
+                    val item = blocks[i - 1].lines[0].text
+                    val price = line.text.slice(IntRange(1, line.text.length - 1)).toFloat()
+                    // display receipt line item
+                    val receiptItem = Pair(item, price)
+                    receiptList.add(receiptItem)
+                }
             }
         }
+        adapter.notifyDataSetChanged()
     }
 
     override fun onResume() {
